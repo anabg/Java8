04b26bd32dd3bf3fa2ac3053590812169606436e
anabg <anitagrimaut@gmail.com>
2018-02-11 22:31:27 -0300

    add comments to describe problem and solutions

--- src/org/java8recipes/chapter01/recipe1_07/AcceptingInput.java
+++ src/org/java8recipes/chapter01/recipe1_07/AcceptingInput.java
@@ -7,6 +7,59 @@ import java.lang.management.BufferPoolMXBean;
 
 /**
  * Created by ÊAnita on 25/1/2017.
+ *
+ * Problem:
+ * You are interested in writing a command-line or terminal application that will accept user input from the keyboard.
+ *
+ * Solution:
+ *
+ * Make use of the java.io.BufferedReader and java.io.InputStramReader classes to read keyboard entry and store
+ * it into local variables. Listing 1-7 shows a program that will keep propting for input until you enter some characters
+ * that represent a valid value of type long.
+ *
+ * How It Works:
+ *
+ * Quite often our applications need to accept user input of some kind. Granted, most applications are not used from
+ * the command-line or terminal nowadays, but having the ability to create an application that reads input from the
+ * command-line or terminal helps to lay a good foundation, and may be useful in some applications. Terminal input
+ * can also be useful un developing administrative applications tha you or a system administrator may use.
+ * Two helper classes were used in the solution to this recipe. They are java.io.BufferedReader and
+ * java.io.InputStreamReader. The early portion of the code that's using those classes is especially important to
+ * understand:
+ *
+ * BufferedReader readIn = new BufferedReader( new InputStreamReader(System.in));
+ *
+ * The innermost object in this statement is Syste.in. It represents the keyboard. You do not need to declare
+ * System.in. Javas's runtime environment creates the object for you. It is simply "there" to be used.
+ * Syste.in provides access to raw bytes of data from the input device, which is the keyboard in our example. It is
+ * the job of the InputStreamReader class to take tose bytes and convert them into characters in your current character
+ * set. System.in is passed to the InputStreamReader() constructor to create an InputStreamReader object.
+ * InputStramReader knows about characters, but not about lines. It is the BufferedReader class's job to detect
+ * line break's in the input stream, and to enable you to conveniently red a line at a time. BufferedReader also aids
+ * efficiency by allowing physical reads from the input device to be done in different-size chunks than by which your
+ * application consumes the data. This aspect can make a difference when the input stram is a large file rather than the
+ * keyboard.
+ * Following is how the program in Listing 1-7 makes use of an instance (named readIn) of the BufferedReader
+ * class to read a line of input from the keyboard:
+ *
+ * numberAsString = readIn.readLine();
+ *
+ * Executing this statement triggers the following sequene:
+ * 1. System.in returns a sequence of bytes.
+ * 2. InputStreamReader converts those bytes into characters
+ * 3. BufferedReader breaks the character stream into lines input.
+ * 4. readLine() returns one line of input to the application.
+ *
+ * I/O calls must be wrapped in try...catch blocks. These blocks catch any exceptions that may occur. The try part
+ * in the example will fail in the event a conversion is unsuccessful. A failure prevents the numberIsValid flag from being
+ * set to true, which causes the do loop to make another iteration so that user can try again at entering a valid value.
+ * The following statement at the top of Listing 1-7 deserves some mention:
+ * import java.io.*;
+ *
+ * This statement makes available the classes and the methods defined in the java.io package. These include
+ * InputStreamReader and BufferedReader. Also included is the IOException class used in the firt try...catch block.
+ *
+ *
  */
 public class AcceptingInput {
 
