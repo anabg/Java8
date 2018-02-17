8e1183ceba211b52ce4f4f2304dfcc471de38ae4
anabg <anitagrimaut@gmail.com>
2018-02-17 12:39:05 -0300

    modify by refactor

--- src/org/java8recipes/chapter02/recipe2_03/Volume.java
+++ src/org/java8recipes/chapter02/recipe2_03/Volume.java
@@ -10,6 +10,23 @@ package org.java8recipes.chapter02.recipe2_03;
  * the interface declaration and provide a method implementation. The following interface Volume includes a default
  * method implementation for the calculateVolume method:
  *
+ * How It Works
+ *
+ * A standard Java interface consists of method declarations that do not contain a body. Classes implement Java
+ * interfaces, and in doing so they provide implementations for the method declarations of the interface. Java interfaces
+ * are an important part of object orientation, as they abstract implementation details and provide a facade to extend
+ * functionality. In Java 8, interfaces have been changed to allow default method implementations. A default method is
+ * a method declaration and implementation, and it is denoted with the default keyword preceding the declaration.
+ * Any class that extends an interface containing a default method can choose to implement and override the default
+ * implementation or simply use the default implementation.
+ * You may wonder why this functionality was provided in Java 8. Why not simply use abstract classes, rather than
+ * default methods? Default methods were added in order to provide a way to change interfaces without breaking
+ * backward compatibility. Many interfaces needed to be changed to accommodate Java 8 API updates, and in doing
+ * so, these interfaces would possibly break functionality of code that was written for prior releases of Java.
+ * By adding default methods to interfaces, engineers were able to add new functionality to existing interfaces without breaking the
+ * code that relied on those interfaces. To learn more about default methods, refer to Recipe 5-7.
+ *
+ *
  * Created by ÊBelu on 13/2/2017.
  */
 public interface Volume {
