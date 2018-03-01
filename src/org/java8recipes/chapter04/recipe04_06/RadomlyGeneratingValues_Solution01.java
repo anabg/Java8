5341df8c75f8c42c8456c72c6d10cfeb32ae024c
anabg <anitagrimaut@gmail.com>
2018-03-01 00:22:37 -0300

    add recipe 04-06

--- src/org/java8recipes/chapter04/recipe04_06/RadomlyGeneratingValues_Solution01.java
+++ src/org/java8recipes/chapter04/recipe04_06/RadomlyGeneratingValues_Solution01.java
@@ -1,9 +1,38 @@
 package org.java8recipes.chapter04.recipe04_06;
 
+import java.util.Random;
+
 /**
  *
  *
  * Created by ÊBelu on 1/3/2018.
  */
 public class RadomlyGeneratingValues_Solution01 {
+
+    public static void main(String[] args) {
+
+        // Create a new instance of the Random class
+        Random random = new Random();
+        // Generates a random Integer int myInt = random.nextInt();
+
+        // Generates a random Double value
+        double myDouble = random.nextDouble();
+
+        // Generates a random float
+        float myFloat = random.nextFloat();
+
+        // Generates a random Gaussian double
+        // mean 0.0 and standard deviation 1.0
+        // from this random number generator's sequence.
+        double gausDouble = random.nextGaussian();
+
+        // Generates a random Long
+        long myLong = random.nextLong();
+
+        // Generates a random boolean
+        boolean myBoolean = random.nextBoolean();
+
+
+
+    }
 }
--- src/org/java8recipes/chapter04/recipe04_06/RadomlyGeneratingValues_Solution02.java
+++ src/org/java8recipes/chapter04/recipe04_06/RadomlyGeneratingValues_Solution02.java
@@ -4,4 +4,12 @@ package org.java8recipes.chapter04.recipe04_06;
  * Created by æAnita on 1/3/2018.
  */
 public class RadomlyGeneratingValues_Solution02 {
+
+    public static void main(String[] args) {
+
+        double rand = Math.random();
+
+        System.out.println("random number " + rand);
+
+    }
 }
