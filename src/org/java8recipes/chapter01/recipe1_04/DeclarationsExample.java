c0293b650318f7d724eb37c2e2b77890e3367069
Grimaut, Ana <Ana_Grimaut@McAfee.com>
2018-04-16 10:28:33 -0300

    add modification in recipe 01-04

--- src/org/java8recipes/chapter01/recipe1_04/DeclarationsExample.java
+++ src/org/java8recipes/chapter01/recipe1_04/DeclarationsExample.java
@@ -7,7 +7,7 @@ package org.java8recipes.chapter01.recipe1_04;
  * the String class type. Listing 1-2 shows an example declaration of each. Draw from the
  * example to declare the variables needed in your own application.
  * <p>
- * Created by æBelu on 25/1/2017.
+ * Created by ï¿½Belu on 25/1/2017.
  */
 public class DeclarationsExample {
 
@@ -15,13 +15,26 @@ public class DeclarationsExample {
         boolean BooleanVal = true;  /* Default is false */
         char charval = 'G';     /* Unicode UTF-16 */
         charval = '\u0490';     /* Ukrainian letter Ghe(?) */
-        byte byteval;       /*  8 bits, -127 to 127 */
-        short shortval;     /* 16 bits, -32,768 to 32,768 */
-        int intval;         /* 32 bits, -2147483648 to 2147483647 */
-        long longval;       /* 64 bits, -(2^64) to 2^64 - 1 */
+        byte byteval = 16;       /*  8 bits, -127 to 127 */
+        short shortval = 16000;     /* 16 bits, -32,768 to 32,768 */
+        int intval = 2147483647;         /* 32 bits, -2147483648 to 2147483647 */
+        long longval = 2147483680L;       /* 64 bits, -(2^64) to 2^64 - 1 */
         float floatval = 10.123456F; /* 32-bit IEEE 754 */
         double doubleval = 10.12345678987654; /* 64-bit IEEE 754 */
         String message = "Darken the corner where you are!";
         message = message.replace("Darken", "Brighten");
+
+
+        System.out.println("boolean Val " + BooleanVal);
+        System.out.println("char val " + charval);
+        System.out.println("byte val " + byteval);
+        System.out.println("short val " + shortval);
+        System.out.println("int val " + intval);
+        System.out.println("long val " + longval);
+        System.out.println("float val " + floatval);
+        System.out.println("double val " + doubleval);
+        System.out.println("message " + message);
+
+
     }
 }
